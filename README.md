7fe4b87ecd896105b0d8c7927e8dba2a0a763e4c
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2022-06-03 10:31:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# express-localhost
+
+```bash
+$ npm install
+$ node index.js
+# Setup internet sharing as described in
+# Open localhost.local:7576 on your device
+# Expexted: display "Not found"
+# Actual(12.4): Connection timed out
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+// Route for debugging purposes
+app.all("*", (request, response) => {
+  console.log(
+    [
+      `Got unhandled request ${request.method} ${request.url}`,
+      "Headers",
+      JSON.stringify(request.headers, null, 2),
+      "Body",
+      JSON.stringify(request.body, null, 2),
+    ].join("\n")
+  );
+  response.sendStatus(404);
+});
+
+app.listen(7576, () => {
+  console.log("Started APERF server on port 7576");
+});
